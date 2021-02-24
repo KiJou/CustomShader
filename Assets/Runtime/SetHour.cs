4d7957c4a96438183623c96dec4bd2ed4618ef1b
Artyom Sovetnikov <elringus@gmail.com>
2021-02-24 20:36:36 +0300

    Initial commit

---
+++ Assets/Runtime/SetHour.cs
@@ -0,0 +1,15 @@
+﻿using Naninovel;
+using UniRx.Async;
+
+[CommandAlias("hour")]
+public class SetHour : Command
+{
+    [ParameterAlias(NamelessParameterAlias), RequiredParameter]
+    public DecimalParameter Hour;
+    public DecimalParameter Duration = 1;
+
+    public override async UniTask ExecuteAsync (CancellationToken cancellationToken = default)
+    {
+        await TimeOfDay.SetHourAsync(Hour, Duration, cancellationToken);
+    }
+}
---
+++ Assets/Runtime/TimeOfDay.cs
@@ -0,0 +1,43 @@
+﻿using Naninovel;
+using UniRx.Async;
+using UnityEngine;
+
+public class TimeOfDay : MonoBehaviour
+{
+    public static float CurrentHour { get; private set; }
+
+    [SerializeField] private new Light light = default;
+    [SerializeField] private Gradient lightGradient = default;
+    [SerializeField] private AnimationCurve emissionGradient = default;
+
+    private static readonly int emissionId = Shader.PropertyToID("_Emission");
+    private static readonly Tweener<FloatTween> tweener = new Tweener<FloatTween>();
+    private static TimeOfDay instance;
+
+    public static void SetHour (float hour)
+    {
+        CurrentHour = hour % 24;
+
+        var gradient = CurrentHour / 24;
+
+        var lightColor = instance.lightGradient.Evaluate(gradient);
+        instance.light.color = lightColor;
+
+        var emission = instance.emissionGradient.Evaluate(gradient);
+        Shader.SetGlobalFloat(emissionId, emission);
+    }
+
+    public static async UniTask SetHourAsync (float hour, float duration,
+        CancellationToken cancellationToken = default)
+    {
+        if (tweener.Running) tweener.CompleteInstantly();
+        var tween = new FloatTween(CurrentHour, hour, duration, SetHour, target: instance);
+        await tweener.RunAsync(tween, cancellationToken);
+    }
+
+    private void Awake ()
+    {
+        instance = FindObjectOfType<TimeOfDay>();
+        SetHour(0);
+    }
+}
